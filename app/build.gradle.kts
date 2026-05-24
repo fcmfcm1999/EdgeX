d0ab6c6ace0f2dc7985dba52926af0d43ee0a77d
0xFantasy <fcmfantasy1999@gmail.com>
2026-05-24 18:38:33 +0800

    chore: enable compose ui stack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 val localProperties = Properties().apply {
@@ -27,6 +28,7 @@ android {
     buildFeatures {
         buildConfig = true
         aidl = true
+        compose = true
     }
 
     defaultConfig {
@@ -97,9 +99,19 @@ dependencies {
     implementation(project(":premium-api"))
     implementation("com.github.topjohnwu.libsu:core:6.0.0")
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 }
